cd40c1586442ce4f8252774525b9d4f136d79337
Arthur <prishep.art@gmail.com>
2025-10-16 17:57:41 +0300

    Кінець 5 уроку + ДЗ 5.1 (допрацював, знову)

--- 5HillelLesson/home_task1.py
+++ 5HillelLesson/home_task1.py
@@ -4,12 +4,7 @@ import keyword
 s = input("Введіть рядок, який міг би бути ім'ям змінної: ")
 
 
-if set(s) == {"_"}: # я тут також шукав і питав у ШІ, яку треба ф-цію, щоб вона брала елементи, без повторів - set()
-    if len(s) == 1:
-        print("True, ім'я змінної допустимо!")
-    else:
-        print("False, ім'я змінної не може мати в собі більше ніж 1 елемент '_'!")
-elif s[0].isdigit():
+if s[0].isdigit():
     print("False, ім'я змінної не може починатися з цифри!")
 elif any(w.isupper() for w in s):
     print("False, ім'я змінної не може містити в собі великі літери!")
@@ -17,6 +12,8 @@ elif any(p in string.punctuation and p != "_" for p in s) or " " in s:
     print("False, ім'я змінної не може містити пробіл і знаки пунктуації в собі!")
 elif s in keyword.kwlist:
     print("False, ім'я змінної не може бути вже зареєстрованним словом!")
+elif any(s[i] == s[i+1] == '_' for i, _ in enumerate(s[:-1])):
+    print("False, ім'я змінної не може містити два підряд '_'!")
 
 else:
     print("True, ім'я змінної допустимо!")

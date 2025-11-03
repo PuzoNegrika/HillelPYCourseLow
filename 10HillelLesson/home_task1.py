8fd2b05027e36b310ef66adf38ebca56c6409732
Arthur <prishep.art@gmail.com>
2025-11-03 17:07:45 +0200

    Кінець 10 уроку + ДЗ 10.1

--- 10HillelLesson/home_task1.py
+++ 10HillelLesson/home_task1.py
@@ -15,7 +15,6 @@ def some_gen(begin, end, func):
         begin = func(begin)
         count += 1
 
-
 gen = some_gen(2, 4, pow)
 assert isgenerator(gen) == True, 'Test1'
 assert list(gen) == [2, 4, 16, 256], 'Test2'

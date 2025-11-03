eed532ee93790d29f6e5d03c8d6dae2d3318b7bf
Arthur <prishep.art@gmail.com>
2025-11-03 17:18:06 +0200

    Кінець 10 уроку + ДЗ 10.2

---
+++ 10HillelLesson/home_task2.py
@@ -0,0 +1,21 @@
+def first_word(text):
+    """ Пошук першого слова """
+    i = 0
+    while i < len(text) and text[i] in " .,":
+        i += 1
+
+    word = ""
+    while i < len(text) and (text[i].isalpha() or text[i] == "'"):
+        word += text[i]
+        i += 1
+
+    return word
+
+
+assert first_word("Hello world") == "Hello", 'Test1'
+assert first_word("greetings, friends") == "greetings", 'Test2'
+assert first_word("don't touch it") == "don't", 'Test3'
+assert first_word(".., and so on ...") == "and", 'Test4'
+assert first_word("hi") == "hi", 'Test5'
+assert first_word("Hello.World") == "Hello", 'Test6'
+print('OK')

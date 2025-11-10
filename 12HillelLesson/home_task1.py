4772b85d1d3474867c377add58f78921803c9970
Arthur <prishep.art@gmail.com>
2025-11-10 21:34:59 +0200

    Кінець 12 уроку + ДЗ 12.1 оновлена версія

--- 12HillelLesson/home_task1.py
+++ 12HillelLesson/home_task1.py
@@ -5,7 +5,16 @@ def delete_html_tags(html_file, result_file='cleaned.txt'):
     with codecs.open(html_file, 'r', 'utf-8') as file:
           html = file.read()
 
-    result = re.sub(r'<.*?>', '', html) # сам пайчарм підказав, але я можу зробити з циклом, по типу: for char in html, якщо це рішення не правильне
+    result = ''
+    tag = False
 
-    with open (result_file, 'w', encoding='utf-8') as f:
+    for char in html:
+        if char == '<':
+            tag = True
+        elif char == '>':
+            tag = False
+        elif not tag:
+            result += char
+
+    with open(result_file, 'w', encoding='utf-8') as f:
         f.write(result)
